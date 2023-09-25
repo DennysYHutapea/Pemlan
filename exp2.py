4b5616db6c49ceb4fa7d5161bd960b11e4bf4bc9
samueltommy <samuel_tommys@yahoo.com>
2023-09-25 12:45:51 +0700

    masih belom jalan 100%

--- exp2.py
+++ exp2.py
@@ -1,4 +1,101 @@
+import tkinter as tk
+from tkinter import ttk
 import mysql.connector
+from prettytable import PrettyTable
+import csv
+
+# Global variable to track whether the main menu is displayed
+main_menu_displayed = False
+
+# Global variable for database connection
+conn = None
+
+# Global variable for current database
+current_database = None
+
+# Function to show the main menu using GUI
+def show_main_menu():
+    global main_menu_displayed, current_database
+    main_menu_displayed = True
+
+    # Remove any previous widgets
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    # Create and pack labels for your menu text
+    label1 = ttk.Label(root, text="....Koneksi ke MySQL dimulai...")
+    label1.pack()
+
+    label2 = ttk.Label(root, text="Daftar Basis Data:")
+    label2.pack()
+
+    # Replace the following list with your actual database list
+    databases = ["basis1", "information_schema", "mysql", "performance_schema", "sys"]
+
+    for i, db in enumerate(databases, start=1):
+        db_label = ttk.Label(root, text=f"{i}. {db}")
+        db_label.pack()
+
+    label3 = ttk.Label(root, text="Pilihan:")
+    label3.pack()
+
+    label4 = ttk.Label(root, text="1. Pilih basis data untuk melihat isinya.")
+    label4.pack()
+
+    label5 = ttk.Label(root, text="2. Tambah basis data baru.")
+    label5.pack()
+
+    label6 = ttk.Label(root, text="3. Hapus basis data.")
+    label6.pack()
+
+    label7 = ttk.Label(root, text="4. Tambah data ke tabel.")
+    label7.pack()
+
+    label8 = ttk.Label(root, text="5. Tampilkan data dari tabel.")
+    label8.pack()
+
+    label9 = ttk.Label(root, text="6. Ubah nama database.")
+    label9.pack()
+
+    label10 = ttk.Label(root, text="7. Ubah nama kolom di tabel.")
+    label10.pack()
+
+    label11 = ttk.Label(root, text="8. Jadikan kolom pertama sebagai primary key.")
+    label11.pack()
+
+    label12 = ttk.Label(root, text="9. Menyimpan Data yang Sudah Ada.")
+    label12.pack()
+
+    label13 = ttk.Label(root, text="10. Keluar.")
+    label13.pack()
+
+    # Create an entry widget for user input
+    input_entry = ttk.Entry(root)
+    input_entry.pack()
+
+    # Create a button to capture the user's choice
+    submit_button = ttk.Button(root, text="Pilih operasi", command=lambda: process_choice(input_entry.get()))
+    submit_button.pack()
+
+# Function to switch to the database selection screen
+def show_database_selection():
+    global current_database
+
+    # Remove the previous widgets
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    # Create a new frame for the database selection screen
+    database_selection_frame = ttk.Frame(root)
+    database_selection_frame.pack()
+
+    # Populate the frame with database options
+    databases = show_databases()
+    for i, db in enumerate(databases, start=1):
+        db_button = ttk.Button(database_selection_frame, text=db, command=lambda db=db: show_table_selection(db))
+        db_button.grid(row=i, column=0, padx=10, pady=5)
+
+    current_database = None  # Reset current database
 
 # Mapping tipe data MySQL ke deskripsi yang lebih dimengerti
 type_mapping = {
@@ -16,290 +113,131 @@ table_column_types = {
     # Define column types for other tables as needed
 }
 
-# Connect to MySQL
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="0225400405",
-)
+def connect_to_mysql():
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="0225400405",
+        )
+        if conn.is_connected():
+            print("....Koneksi ke MySQL dimulai...")
+            return conn
+    except mysql.connector.Error as err:
+        print(f"Kesalahan koneksi ke MySQL: {err}")
+    return None
 
-# Check if the connection is successful
-if conn.is_connected():
-    print("....Koneksi ke MySQL dimulai...")
+def show_databases(conn):
+    cursor = conn.cursor()
+    cursor.execute("SHOW DATABASES")
+    return cursor.fetchall()
 
-    while True:
-        # Get the cursor object
-        cursor = conn.cursor()
+# Function to process the user's choice
+def process_choice(choice):
+    databases = show_databases(conn)
+    pilihan = list(range(1, len(databases) + 1))
 
-        # Execute the `SHOW DATABASES` query
-        cursor.execute("SHOW DATABASES")
-
-        # Get the list of databases
-        databases = cursor.fetchall()
-
-        # Print the list of databases
-        print("\nDaftar Basis Data:")
-        for i, db in enumerate(databases, start=1):
-            print(f"{i}. {db[0]}")
-
-        # Buat list pilihan
-        pilihan = list(range(1, len(databases) + 1))
-
-        # Minta user memilih basis data atau operasi
-        print("\nPilihan:")
-        print("1. Pilih basis data untuk melihat isinya.")
-        print("2. Tambah basis data baru.")
-        print("3. Hapus basis data.")
-        print("4. Tambah data ke tabel.")
-        print("5. Tampilkan data dari tabel.")
-        print("6. Keluar.")
-        choice = input("Pilih operasi (1/2/3/4/5/6): ")
-
-        if choice == "1":
-            # Minta user memilih basis data
-            db_choice = input("Pilih basis data yang ingin dilihat ({}): ".format(", ".join(map(str, pilihan))))            
-            try:
-                db_choice = int(db_choice)
-                if db_choice not in pilihan:
-                    print("Pilihan tidak valid.")
-                    continue
-                database = databases[db_choice - 1][0]
-            except ValueError:
-                print("Pilihan harus berupa angka.")
-                continue
-
-            # Lihat isi dari basis data yang dipilih
-            cursor.execute("USE {}".format(database))
-            cursor.execute("SHOW TABLES")
-
-            tables = cursor.fetchall()
-
+    if choice == "1":
+        db_choice = input("Pilih basis data yang ingin dilihat isinya ({})".format(", ".join(map(str, pilihan))))
+        try:
+            db_choice = int(db_choice)
+            if db_choice not in pilihan:
+                print("Pilihan tidak valid.")
+            database = databases[db_choice - 1][0]
+            tables = gui_list_tables(conn, database)
             print("\nIsi database {}:".format(database))
             for table in tables:
-                cursor.execute("SELECT * FROM {}".format(table[0]))
-
+                print("\nTabel '{}' di basis data '{}':".format(table, database))
+                cursor = conn.cursor()
+                cursor.execute(f"USE {database}")
+                cursor.execute(f"SELECT * FROM {table}")
                 rows = cursor.fetchall()
-
+                table_obj = PrettyTable()
+                table_obj.field_names = [column[0] for column in cursor.description]
                 for row in rows:
-                    print(row)
+                    table_obj.add_row(row)
+                print(table_obj)
+        except ValueError:
+            print("Pilihan harus berupa angka.")
+    elif choice == "2":
+        # Implement the logic for option 2 (Tambah basis data baru) here
+        pass
+    elif choice == "3":
+        # Implement the logic for option 3 (Hapus basis data) here
+        pass
+    elif choice == "4":
+        # Implement the logic for option 4 (Tambah data ke tabel) here
+        pass
+    elif choice == "5":
+        # Implement the logic for option 5 (Tampilkan data dari tabel) here
+        pass
+    elif choice == "6":
+        # Implement the logic for option 6 (Ubah nama database) here
+        pass
+    elif choice == "7":
+        # Implement the logic for option 7 (Ubah nama kolom di tabel) here
+        pass
+    elif choice == "8":
+        # Implement the logic for option 8 (Jadikan kolom pertama sebagai primary key) here
+        pass
+    elif choice == "9":
+        # Implement the logic for option 9 (Menyimpan Data yang Sudah Ada) here
+        pass
+    elif choice == "10":
+        quit_program()  # Call the function to exit the program
 
-            # Tampilkan pesan keterangan ketika kembali ke halaman awal
-            print("\nKembali ke halaman awal...")
+# Function to switch to the database selection screen
+def show_database_selection():
+    global current_database
 
-        elif choice == "2":
-            # Minta user untuk nama basis data baru
-            new_database = input("Masukkan nama basis data baru: ")
+    # Remove the previous widgets
+    for widget in root.winfo_children():
+        widget.destroy()
 
-            # Buat basis data baru jika belum ada
-            cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(new_database))
-            print("Basis data '{}' berhasil ditambahkan.".format(new_database))
+    # Create a new frame for the database selection screen
+    database_selection_frame = ttk.Frame(root)
+    database_selection_frame.pack()
 
-            # Commit perubahan untuk membuat basis data baru
-            conn.commit()
+    # Populate the frame with database options
+    databases = show_databases()
+    for i, db in enumerate(databases, start=1):
+        db_button = ttk.Button(database_selection_frame, text=db, command=lambda db=db: show_table_selection(db))
+        db_button.grid(row=i, column=0, padx=10, pady=5)
 
-            # Minta user untuk membuat tabel di basis data baru
-            create_table = input("Buat tabel di basis data '{}'? (y/n): ".format(new_database))
+    current_database = None  # Reset current database
 
-            if create_table.lower() == 'y':
-                # Menampilkan pilihan sintaks SQL untuk membuat tabel
-                print("Pilihan Sintaks SQL untuk Membuat Tabel:")
-                print("1. Membuat tabel dengan kolom bilangan bulat dan teks")
-                print("2. Membuat tabel dengan kolom bilangan bulat, teks, dan primary key")
+# Function to switch to the table selection screen
+def show_table_selection(database):
+    global current_database
 
-                table_syntax = input("Pilih sintaks yang ingin digunakan (1/2): ")
+    # Remove the previous widgets
+    for widget in root.winfo_children():
+        widget.destroy()
 
-                if table_syntax == "1":
-                    table_definition = """
-                    CREATE TABLE table_name (
-                        column1 INT,
-                        column2 VARCHAR(255)
-                    )
-                    """
-                elif table_syntax == "2":
-                    table_definition = """
-                    CREATE TABLE table_name (
-                        id INT AUTO_INCREMENT PRIMARY KEY,
-                        column1 INT,
-                        column2 VARCHAR(255)
-                    )
-                    """
-                else:
-                    print("Sintaks yang dipilih tidak valid.")
-                    continue
+    # Create a new frame for the table selection screen
+    table_selection_frame = ttk.Frame(root)
+    table_selection_frame.pack()
 
-                # Mengganti 'table_name' dengan nama tabel yang diinginkan
-                table_name = input("Masukkan nama tabel: ")
-                table_definition = table_definition.replace("table_name", table_name)
+    # Populate the frame with table options
+    tables = gui_list_tables(database)
+    for i, table in enumerate(tables, start=1):
+        table_button = ttk.Button(table_selection_frame, text=table, command=lambda table=table: show_table_data(database, table))
+        table_button.grid(row=i, column=0, padx=10, pady=5)
 
-                # Eksekusi perintah SQL untuk membuat tabel
-                cursor.execute("USE {}".format(new_database))
-                cursor.execute(table_definition)
-                print("Tabel '{}' berhasil dibuat di basis data '{}'.".format(table_name, new_database))
+    current_database = database
 
-                # Commit perubahan untuk membuat tabel baru
-                conn.commit()
+# Function to exit the program gracefully
+def quit_program():
+    global main_menu_displayed  # Access the global variable
+    main_menu_displayed = False
+    root.quit()  # Exit the Tkinter main loop
 
-            # Tampilkan pesan keterangan ketika kembali ke halaman awal
-            print("\nKembali ke halaman awal...")
+# Define the main window
+root = tk.Tk()
+root.title("MySQL Database Manager")
 
-        elif choice == "3":
-            # Minta user untuk nama basis data yang akan dihapus
-            del_database = input("Masukkan nama basis data yang akan dihapus: ")
+# Start by showing the main menu using GUI
+show_main_menu()
 
-            # Hapus basis data jika ada
-            cursor.execute("DROP DATABASE IF EXISTS {}".format(del_database))
-            print("Basis data '{}' berhasil dihapus.".format(del_database))
-
-            # Commit perubahan untuk menghapus basis data
-            conn.commit()
-
-            # Tampilkan pesan keterangan ketika kembali ke halaman awal
-            print("\nKembali ke halaman awal...")
-
-        elif choice == "4":
-            # Minta user memilih basis data
-            db_choice = input("Pilih basis data tempat tabel berada ({})".format(", ".join(map(str, pilihan))))
-            try:
-                db_choice = int(db_choice)
-                if db_choice not in pilihan:
-                    print("Pilihan tidak valid.")
-                    continue
-                database = databases[db_choice - 1][0]
-            except ValueError:
-                print("Pilihan harus berupa angka.")
-                continue
-
-            # Minta user memilih tabel untuk menambahkan data
-            cursor.execute("USE {}".format(database))
-            cursor.execute("SHOW TABLES")
-
-            tables = cursor.fetchall()
-
-            print("\nDaftar Tabel di database {}:".format(database))
-            for i, table in enumerate(tables, start=1):
-                print(f"{i}. {table[0]}")
-
-            table_choice = input("Masukkan nomor tabel tempat Anda ingin menambahkan data: ")
-            try:
-                table_choice = int(table_choice)
-                if table_choice < 1 or table_choice > len(tables):
-                    print("Nomor tabel tidak valid.")
-                    continue
-                table_name = tables[table_choice - 1][0]
-            except ValueError:
-                print("Nomor tabel harus berupa angka.")
-                continue
-
-            # Cek apakah tabel ada di basis data yang dipilih
-            if table_name not in [table[0] for table in tables]:
-                print("Tabel '{}' tidak ditemukan di basis data '{}'.".format(table_name, database))
-                continue
-
-            # Minta user memasukkan data ke dalam tabel
-            print("Masukkan data ke dalam tabel (pisahkan dengan koma, contoh: nilai1, nilai2)")
-
-            # Ambil definisi kolom dari tabel yang sebenarnya
-            cursor.execute("DESCRIBE {}".format(table_name))
-            columns = cursor.fetchall()
-            column_names = [col[0] for col in columns]
-
-            data_values = input("Data: ").split(",")
-
-            if len(data_values) != len(column_names):
-                print("Jumlah nilai yang dimasukkan tidak sesuai dengan jumlah kolom dalam tabel.")
-                continue
-
-            insert_values = []
-            for i in range(len(column_names)):
-                column_name = column_names[i]
-                column_type = table_column_types.get(column_name, "teks")  # Menggunakan teks sebagai tipe data default
-                value = data_values[i].strip()
-                try:
-                    if column_type.startswith("bilangan bulat"):
-                        insert_values.append(int(value))
-                    else:
-                        insert_values.append(value)
-                except ValueError:
-                    print("Kesalahan: Nilai '{}' tidak sesuai dengan tipe data '{}'.".format(value, column_type))
-                    continue
-
-            # Buat query SQL untuk menambahkan data ke tabel
-            placeholders = ", ".join(["%s"] * len(insert_values))
-            insert_query = "INSERT INTO {} ({}) VALUES ({})".format(table_name, ", ".join(column_names), placeholders)
-
-            try:
-                # Eksekusi perintah SQL untuk menambahkan data
-                cursor.execute(insert_query, insert_values)
-                print("Data berhasil ditambahkan ke tabel '{}'.".format(table_name))
-
-                # Commit perubahan untuk menambahkan data
-                conn.commit()
-            except mysql.connector.Error as err:
-                print("Gagal menambahkan data: {}".format(err))
-
-            # Tampilkan pesan keterangan ketika kembali ke halaman awal
-            print("\nKembali ke halaman awal...")
-
-        elif choice == "5":
-            # Minta user memilih basis data
-            db_choice = input("Pilih basis data tempat tabel berada ({})".format(", ".join(map(str, pilihan))))
-            try:
-                db_choice = int(db_choice)
-                if db_choice not in pilihan:
-                    print("Pilihan tidak valid.")
-                    continue
-                database = databases[db_choice - 1][0]
-            except ValueError:
-                print("Pilihan harus berupa angka.")
-                continue
-
-            # Minta user memilih tabel untuk menampilkan data
-            cursor.execute("USE {}".format(database))
-            cursor.execute("SHOW TABLES")
-
-            tables = cursor.fetchall()
-
-            print("\nDaftar Tabel di database {}:".format(database))
-            for i, table in enumerate(tables, start=1):
-                print(f"{i}. {table[0]}")
-
-            table_choice = input("Masukkan nomor tabel tempat Anda ingin menampilkan data: ")
-            try:
-                table_choice = int(table_choice)
-                if table_choice < 1 or table_choice > len(tables):
-                    print("Nomor tabel tidak valid.")
-                    continue
-                table_name = tables[table_choice - 1][0]
-            except ValueError:
-                print("Nomor tabel harus berupa angka.")
-                continue
-
-            # Cek apakah tabel ada di basis data yang dipilih
-            if table_name not in [table[0] for table in tables]:
-                print("Tabel '{}' tidak ditemukan di basis data '{}'.".format(table_name, database))
-                continue
-
-            # Tampilkan isi dari tabel yang dipilih
-            cursor.execute("SELECT * FROM {}".format(table_name))
-
-            rows = cursor.fetchall()
-
-            print("\nIsi tabel '{}' di basis data '{}':".format(table_name, database))
-            for row in rows:
-                print(row)
-
-            # Tampilkan pesan keterangan ketika kembali ke halaman awal
-            print("\nKembali ke halaman awal...")
-
-        elif choice == "6":
-            # Keluar dari loop tak terbatas
-            break
-
-        else:
-            print("Operasi tidak valid.")
-
-    print("\n....Koneksi ke MySQL ditutup.....")
-
-    # Close the cursor and connection
-    cursor.close()
-    conn.close()
+# Start the Tkinter main loop
+root.mainloop()
